2be1179b72409da268563f9736af125dfea5a283
hafizsafaraz <hafizsafaraz850@gmail.com>
2026-05-12 16:55:02 +0700

    fix: cross-platform compile args

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 from pybind11.setup_helpers import Pybind11Extension
+import sys
+
+if sys.platform == "win32":
+    compile_args = ["/O2", "/arch:AVX2"]
+else:
+    compile_args = ["-O2", "-mavx2"]   
 
 ext_modules = [
     Pybind11Extension(
         "adept",
         ["src/tensor.cpp", "src/bindings.cpp"],
-        include_dirs=["include"],
-        extra_compile_args=["/O2", "/arch:AVX2"],
+        include_dirs=["include"], 
+        extra_compile_args=compile_args,
     ),
 ]
 
