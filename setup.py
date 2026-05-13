58ac7765d70ad21713fa1f94eceae036dc5e2b30
hafizsafaraz <hafizsafaraz850@gmail.com>
2026-05-13 15:24:10 +0700

    build: add math.cpp and linalg.cpp to build

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 from pybind11.setup_helpers import Pybind11Extension
 
-compile_args = ["-02"]
+compile_args = ["-O2"]
 
 ext_modules = [
     Pybind11Extension(
         "adept",
-        ["src/tensor.cpp", "src/bindings.cpp"],
+        ["src/tensor.cpp", "src/math.cpp", "src/linalg.cpp", "src/bindings.cpp"],
         include_dirs=["include"], 
         extra_compile_args=compile_args,
         cxx_std=17,
