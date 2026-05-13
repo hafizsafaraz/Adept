ac62af314ad7bb03aeae892cbe0d4af10b0c4274
hafizsafaraz <hafizsafaraz850@gmail.com>
2026-05-13 15:01:00 +0700

    fix: add empty tensor guard to sum()

--- src/tensor.cpp
+++ src/tensor.cpp
@@ -63,6 +63,9 @@ int Tensor::ndim() const {
 
 // Sum of all elements
 double Tensor::sum() const {
+    if(data.empty()) {
+        throw std::invalid_argument("Tensor is empty");
+    }
     double sum = 0;
     for(int i = 0; i < size(); i++) {
         sum += data[i];
